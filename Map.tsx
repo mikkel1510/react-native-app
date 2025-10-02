5b63ccdfed2f26af13b3a8b966b90d0d077f41d8
mikkel1510 <144017496+mikkel1510@users.noreply.github.com>
2025-10-02 10:45:13 +0200

    Update Map.tsx

--- Map.tsx
+++ Map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
